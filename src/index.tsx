ab0b7003e4fad9385f825c6543196caa6f791435
joelynzhou <joelynzhou@hotmail.com>
2018-11-25 21:15:13 +0800

    react-router init

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App';
+import Sample from './Sample';
 
 ReactDOM.render(
-  <App />,
+  (
+    <Router >
+        <div>
+            <Route path="/sample" component={Sample}/>
+            <Route path="/app" component={App}/>
+        </div>
+    </Router>
+),
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
